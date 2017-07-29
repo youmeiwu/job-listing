03577e9b7b5e86b287666ff61dbd88f0a7a6ed12
sarah <smile-wu@hotmail.com>
2017-07-29 15:19:25 +0800

    add is_hidden to jobs and apply to view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false)
   end
 
   def new
@@ -44,7 +44,7 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
 
   private
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 end
