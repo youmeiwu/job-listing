5951599a1e9a5fee116689b1ba21dbaf89b27c3a
sarah <smile-wu@hotmail.com>
2017-08-01 12:39:05 +0800

    add publish and hide function

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,10 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This job already archieved"
+      redirect_to root_path
+    end
   end
 
   def edit
